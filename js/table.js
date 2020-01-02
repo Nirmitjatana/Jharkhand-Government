33589f8808e87c938445ff1959f68bdaf4da0ce9
Prakhar Soni <prakhar0912@gmail.com>
2020-01-02 22:42:47 +0530

    table size alterations

--- js/table.js
+++ js/table.js
@@ -243,22 +243,19 @@ function table(){
         {
             var employee_data = data.split(/\r?\n|\r/);
             var table_data = '<table class="table table-bordered table-striped">';
-            for(var count = 0; count<employee_data.length; count++)
-            {
-            var cell_data = employee_data[count].split(",");
-            table_data += '<tr>';
-            for(var cell_count=0; cell_count<cell_data.length; cell_count++)
-            {
-            if(count === 0)
-            {
-            table_data += '<th>'+cell_data[cell_count]+'</th>';
-            }
-            else
-            {
-            table_data += '<td>'+cell_data[cell_count]+'</td>';
-            }
-            }
-            table_data += '</tr>';
+            for(var count = 0; count<employee_data.length; count++){
+                var cell_data = employee_data[count].split(",");
+                console.log(cell_data)
+                table_data += '<tr>';
+                for(var cell_count=0; cell_count<cell_data.length; cell_count++){
+                    if(count === 1){
+                        table_data += '<th>'+cell_data[cell_count]+'</th>';
+                    }
+                    else{
+                        table_data += '<td>'+cell_data[cell_count]+'</td>';
+                    }
+                }
+                table_data += '</tr>';
             }
             table_data += '</table>';
             $(selector).html(table_data);
