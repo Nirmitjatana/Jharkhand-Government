096bd640082793ef4a83b00828207c21569ed8df
Prakhar Soni <prakhar0912@gmail.com>
2020-01-12 09:37:12 +0530

    fixing all the bugs

--- js/download.js
+++ js/download.js
@@ -22,7 +22,8 @@ feed.addEventListener('mouseover', () => {
 feed.addEventListener('mouseout', () => {
   feedbackImg.src = '../Assets/img/feedback.png';
 });
-const save= document.querySelector('.save');
+
+const save = document.querySelector('.save');
 const saveImg = document.querySelector('.saveImg');
 save.addEventListener('mouseover', () => {
   saveImg.src = '../Assets/img/save_pink.png';
@@ -31,31 +32,19 @@ save.addEventListener('mouseout', () => {
   saveImg.src = '../Assets/img/save.png';
 });
 
-const suc = document.querySelector('.suc');
-const sucImg = document.querySelector('.sucImg');
-about.addEventListener('mouseover', () => {
-  sucImg.src = '../Assets/img/about_pink.png';
-});
-suc.addEventListener('mouseout', () => {
-  sucImg.src = '../Assets/img/about.png';
-});
-const eve = document.querySelector('.eve');
-const eveImg = document.querySelector('.eveImg');
-eve.addEventListener('mouseover', () => {
-  eveImg.src = '../Assets/img/about_pink.png';
-});
-eve.addEventListener('mouseout', () => {
-  eveImg.src = '../Assets/img/about.png';
-});
-
 const ham = document.querySelector('.bam');
 const navExp = document.querySelector('.nav_expanded');
 const container = document.querySelector('.container');
+/* const logo2 = document.querySelector('.logo2'); */
+/* console.log(logo2); */
 const logo3 = document.querySelector('.logo3');
 const govt = document.querySelector('.govt');
+const suc = document.querySelector(".Suc");
+const eve = document.querySelector(".eve")
 let i = 0;
 ham.addEventListener('click', okay);
 const t1 = new TimelineLite();
+
 function okay() {
   i++;
   if (i % 2 == 0) {
@@ -63,9 +52,11 @@ function okay() {
     feed.style.display = 'none';
     tables.style.display = 'none';
     save.style.display = 'none';
-    logo2.style.display = 'none';
+    /* logo2.style.display = 'none'; */
     logo3.style.display = 'none';
     govt.style.display = 'none';
+    suc.style.display = 'none';
+    eve.style.display = 'none';
     container.style.gridTemplateRows = '55px auto';
     TweenLite.fromTo(navExp, 1, {
       width: '100%',
@@ -92,8 +83,18 @@ function okay() {
     }, {
       opacity: '0',
     });
-    logo2.style.opacity = '0';
-    logo2.style.display = 'none';
+    TweenLite.fromTo(suc, 0.5, {
+      opacity: '1',
+    }, {
+      opacity: '0',
+    });
+    TweenLite.fromTo(eve, 0.5, {
+      opacity: '1',
+    }, {
+      opacity: '0',
+    });
+    /* logo2.style.opacity = '0';
+    logo2.style.display = 'none'; */
   } else {
     TweenLite.fromTo(about, 0.1, {
       opacity: '0',
@@ -123,30 +124,46 @@ function okay() {
       opacity: '1',
       display: 'block',
     });
-    logo2.style.display = 'block';
-    logo3.style.display = 'block';
-    govt.style.display = 'block';
+    TweenLite.fromTo(suc, 0.1, {
+      opacity: '0',
+      display: 'none',
+    }, {
+      opacity: '1',
+      display: 'block',
+    });
+    TweenLite.fromTo(eve, 0.1, {
+      opacity: '0',
+      display: 'none',
+    }, {
+      opacity: '1',
+      display: 'block',
+    });
+    /* logo2.style.display = 'block'; */
     container.style.gridTemplateRows = '100%';
-    navExp.style.gridTemplateRows = '12% 12% 12% 12% 20% 20% 5%';
+    navExp.style.gridTemplateRows = '12% 12% 12% 12% 12% 12% 18% 5%';
     t1.fromTo(navExp, 1, {
       width: '0',
     }, {
       width: '100%',
     })
-      .fromTo(logo2, 0.1, {
+      /* .fromTo(logo2, 0.1, {
         opacity: '0',
       }, {
         opacity: '1',
-      })
+      }) */
       .fromTo(logo3, 0.1, {
         opacity: '0',
+        display: 'none',
       }, {
         opacity: '1',
+        display: 'block',
       })
       .fromTo(govt, 0.1, {
         opacity: '0',
+        display: 'none',
       }, {
         opacity: '1',
+        display: 'block',
       });
   }
 }
