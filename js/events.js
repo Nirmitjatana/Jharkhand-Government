980cadf05557684ce472521e78ed3615ab922048
Nirmitjatana <Nirmitjatana@users.noreply.github.com>
2020-01-31 12:37:14 +0530

    fetch added

--- js/events.js
+++ js/events.js
@@ -1,20 +1,20 @@
-function openCity(evt, cityName) {
-    var i, x, tablinks;
-    x = document.getElementsByClassName("city");
-    for (i = 0; i < x.length; i++) {
-      x[i].style.display = "none";
-    }
-    tablinks = document.getElementsByClassName("tablink");
-    for (i = 0; i < x.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" w3-border-red", "");
-    }
-    document.getElementById(cityName).style.display = "block";
-    evt.currentTarget.firstElementChild.className += " w3-border-red";
-  }
-  document.getElementById("defaultOpen").click();
-  document.querySelector('a').addEventListener('click', function() {
-    document.querySelector('.smalldesc').classList.toggle('expand');
-  });
+// function openCity(evt, cityName) {
+//     var i, x, tablinks;
+//     x = document.getElementsByClassName("city");
+//     for (i = 0; i < x.length; i++) {
+//       x[i].style.display = "none";
+//     }
+//     tablinks = document.getElementsByClassName("tablink");
+//     for (i = 0; i < x.length; i++) {
+//       tablinks[i].className = tablinks[i].className.replace(" w3-border-red", "");
+//     }
+//     document.getElementById(cityName).style.display = "block";
+//     evt.currentTarget.firstElementChild.className += " w3-border-red";
+//   }
+//   document.getElementById("defaultOpen").click();
+//   document.querySelector('a').addEventListener('click', function() {
+//     document.querySelector('.smalldesc').classList.toggle('expand');
+//   });
   const tables = document.querySelector('.tables');
 const tablesImg = document.querySelector('.tablesImg');
 tables.addEventListener('mouseover', () => {
